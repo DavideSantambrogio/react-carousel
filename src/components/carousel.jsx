c219de8adef69832f858ac3a758625828cb75fab
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-06-14 16:30:20 +0200

    add autoplay

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import slides from "../data/data";
 
-
 const Carousel = () => {
     const [activeSlide, setActiveSlide] = useState(0);
+    const autoplayDelay = 1500;
 
     const handleNextSlide = () => {
         setActiveSlide((prevActiveSlide) => (prevActiveSlide + 1) % slides.length);
@@ -13,50 +13,56 @@ const Carousel = () => {
         setActiveSlide((prevActiveSlide) => (prevActiveSlide - 1 + slides.length) % slides.length);
     };
 
+    useState(() => {
+        const interval = setInterval(() => {
+            setActiveSlide((prevActiveSlide) => (prevActiveSlide + 1) % slides.length);
+        }, autoplayDelay);
+
+        return () => clearInterval(interval); 
+    }, []); 
+
     return (
         <>
-
             <h1>Carosello</h1>
-            <div className="container">
-                <button className="carousel-button" onClick={handlePreviousSlide}>
-                    ←
-                </button>
-                <div className="carousel-container">
-                    <div className="carousel">
-                        {slides.map((slide, index) => (
-                            <div
-                                key={index}
-                                className={`carousel-item 
+                <div className="container">
+                    <button className="carousel-button" onClick={handlePreviousSlide}>
+                        ←
+                    </button>
+                    <div className="carousel-container">
+                        <div className="carousel">
+                            {slides.map((slide, index) => (
+                                <div
+                                    key={index}
+                                    className={`carousel-item 
                                     ${index === activeSlide ? 'active' : ''} 
                                     ${index === activeSlide - 1 || (activeSlide === 0 && index === slides.length - 1) ? 'prev' : ''} 
                                     ${index === activeSlide + 1 || (activeSlide === slides.length - 1 && index === 0) ? 'next' : ''}`
-                                }>
-                                <img src={slide.image} alt={slide.title} className="carousel-image" />
-                                <h1 className='slide-title'>{slide.title}</h1>
-                            </div>
-                        ))}
+                                    }>
+                                    <img src={slide.image} alt={slide.title} className="carousel-image" />
+                                    <h1 className='slide-title'>{slide.title}</h1>
+                                </div>
+                            ))}
+                        </div>
                     </div>
+
+                    <button className="carousel-button" onClick={handleNextSlide}>
+                        →
+                    </button>
                 </div>
 
-                <button className="carousel-button" onClick={handleNextSlide}>
-                    →
-                </button>
-            </div>
-
-
-            <ul className="bullet-navigation">
-                {slides.map((slide, index) => (
-                    <li
-                        key={index}
-                        className={index === activeSlide ? 'active' : ''}
-                        onClick={() => setActiveSlide(index)}
-                    ></li>
-                ))}
-            </ul>
-
+                {/* Bullet Navigation */}
+                <ul className="bullet-navigation">
+                    {slides.map((slide, index) => (
+                        <li
+                            key={index}
+                            className={index === activeSlide ? 'active' : ''}
+                            onClick={() => setActiveSlide(index)}
+                        ></li>
+                    ))}
+                </ul>
+            
         </>
     );
-
 };
 
 export default Carousel;
